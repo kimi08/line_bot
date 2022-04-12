ad79da578ce75c8e658aba19af22f9b5344036d6
Kmii Yu <idiot3878@gmail.com>
2022-04-12 22:48:40 +0800

    update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import urllib
 
 app = Flask(__name__)
 
@@ -36,23 +37,45 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
+
+
+def pixabay_isch(event):
     
-    r = "很抱歉,您說什麼"
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        try:
+            url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+            
+            req = urllib.request.Request(url, headers = headers)
+            conn = urllib.request.urlopen(req)
+            
+            print('fetch page finish')
+            
+            pattern = 'img srcset="\S*\s\w*,'
+            img_list = []
+            
+            for match in re.finditer(pattern, str(conn.read())):
+                img_list.append(match.group()[12:-3])
+                
+            random_img_url = img_list[random.randint(0, len(img_list)+1)]
+            print('fetch img url finish')
+            print(random_img_url)
+            
+            line_bot_api.reply_message(
+                event.reply_token,
+                ImageSendMessage(
+                    original_content_url=random_img_url,
+                    preview_image_url=random_img_url
+                )
+            )
+        # 如果找不到圖，就學你說話
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text)
+            )
+            pass
 
-    if msg in ['hi', 'HI', 'Hi']:
-        r = '嗨'
-    elif msg == '你吃飯了嗎?':
-        r = '還沒'
-    elif msg == '你是誰':
-        r = '我是機器人'
-    elif '訂位' in msg:
-        r = '您想訂位，是嗎?'
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text= r ))
 
 
 if __name__ == "__main__":
